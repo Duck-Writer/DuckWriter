a56d33d381a275db4064fa9af84fdb9602645a92
Duck-Writer <p.travis.west@gmail.com>
2025-08-07 17:25:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,13 +3,12 @@ const FILES_TO_CACHE = [
     '/DuckWriter/index.html',
     '/DuckWriter/icons/duck192.png',
     '/DuckWriter/icons/duck512.png',
-    '/DuckWriter/sounds/tp1.mp3'
-//    '/DuckWriter/sounds/tp1.mp3'
-  //      '/DuckWriter/sounds/tp2.mp3',
-    //    '/DuckWriter/sounds/tp3.mp3',
-      //  '/DuckWriter/sounds/tp4.mp3',
-// '/DuckWriter/sounds/cr1.mp3',
-  //   '/DuckWriter/sounds/sp2.mp3'
+    '/DuckWriter/sounds/tp1.mp3',
+        '/DuckWriter/sounds/tp2.mp3',
+       '/DuckWriter/sounds/tp3.mp3',
+       '/DuckWriter/sounds/tp4.mp3',
+ '/DuckWriter/sounds/cr1.mp3',
+     '/DuckWriter/sounds/sp2.mp3'
 ];
 
 self.addEventListener('install', (event) => {
