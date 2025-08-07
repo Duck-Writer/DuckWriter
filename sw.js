0f8bb2b4a2810d24eb27f10447ca7133150d3fda
Duck-Writer <p.travis.west@gmail.com>
2025-08-07 11:38:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,5 @@
-// Define a name for your cache and the files you want to save
-// const CACHE_NAME = 'duck-writer-v1';
-// const DUCK_FILES_TO_CACHE = [
-//  '/DuckWriter/',
-//  '/index.html',
-//  '/icons/duck192.png',
-//  '/icons/duck512.png',
-
-//  '/sounds/tp1.mp3',
-// '/sounds/tp2.mp3',
-// '/sounds/tp3.mp3',
-// '/sounds/tp4.mp3',
-// '/sounds/cr1.mp3',
-//  '/sounds/sp2.mp3'
-//  ];
-
-
-
-
 const FILES_TO_CACHE = [
     '/' 
-//    '/icons/duck192.png',
-//  '/icons/duck512.png',
-//    '/index.html'
-
 ];
 
 self.addEventListener('install', (event) => {
@@ -32,10 +9,3 @@ self.addEventListener('install', (event) => {
     })
   );
 });
-
-
-self.addEventListener('fetch', (event) => {
-  // For now, do nothing and let the request go to the network
-  return;
-});
-
