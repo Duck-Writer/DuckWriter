e113189480c5379b15bf34e137442ea6e881548b
Duck-Writer <p.travis.west@gmail.com>
2025-08-07 11:17:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,25 @@
 // Define a name for your cache and the files you want to save
-const CACHE_NAME = 'duck-writer-v1';
-const DUCK_FILES_TO_CACHE = [
-  '/DuckWriter/',
-  '/index.html',
-  '/icons/duck192.png',
-  '/icons/duck512.png',
+// const CACHE_NAME = 'duck-writer-v1';
+// const DUCK_FILES_TO_CACHE = [
+//  '/DuckWriter/',
+//  '/index.html',
+//  '/icons/duck192.png',
+//  '/icons/duck512.png',
 
-  '/sounds/tp1.mp3',
- '/sounds/tp2.mp3',
- '/sounds/tp3.mp3',
- '/sounds/tp4.mp3',
- '/sounds/cr1.mp3',
-'/sounds/sp2.mp3'
-];
+//  '/sounds/tp1.mp3',
+// '/sounds/tp2.mp3',
+// '/sounds/tp3.mp3',
+// '/sounds/tp4.mp3',
+// '/sounds/cr1.mp3',
+//  '/sounds/sp2.mp3'
+//  ];
 
-// 1. The 'install' event - Fired when the service worker is first installed.
+// sw.js - an empty but valid service worker
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installing...');
-  
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Service Worker: Caching app shell');
-        return cache.addAll(DUCK_FILES_TO_CACHE);
-      })
-  );
+  console.log('Service Worker installed!');
 });
 
-// 2. The 'fetch' event - Fired every time the app requests a resource (e.g., a file or data).
 self.addEventListener('fetch', (event) => {
-  console.log('Service Worker: Fetching ', event.request.url);
-
-  event.respondWith(
-    // Check the cache first for a match
-    caches.match(event.request)
-      .then((response) => {
-        // If a match is found in the cache, return it.
-        // Otherwise, fetch it from the network.
-        return response || fetch(event.request);
-      })
-  );
+  // For now, do nothing and let the request go to the network
+  return;
 });
